b990597bb4fbcc233012b1261e6f28cd041a50cd
Austin Waddecar <austin.waddecar@gmail.com>
2020-04-28 02:12:53 -0700

    Added comment for addition

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,6 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
+    // plus signs needed to ensure concatenation does not happen with addition
     'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
